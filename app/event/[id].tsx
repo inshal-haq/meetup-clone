5ed0fd8e021a2665ad59d6ef328e6dc823444d5a
Inshal Haq <inshalulhaq11@gmail.com>
2024-12-26 14:37:02 -0600

    Add event details content

--- app/event/[id].tsx
+++ app/event/[id].tsx
@@ -1,5 +1,6 @@
-import { useLocalSearchParams } from 'expo-router';
-import { View, Text } from 'react-native';
+import dayjs from 'dayjs';
+import { Stack, useLocalSearchParams } from 'expo-router';
+import { View, Text, Image } from 'react-native';
 
 import events from '~/assets/events.json'; // dummy data
 
@@ -7,9 +8,27 @@ export default function EventScreen() {
   const { id } = useLocalSearchParams();
   const event = events.find((event) => event.id === id);
 
+  if (!event) {
+    return <Text>Event not found</Text>;
+  }
+
   return (
-    <View>
-      <Text>Event title: {event?.title}</Text>
+    <View className="flex-1 gap-3 bg-white p-3">
+      <Stack.Screen
+        options={{
+          headerTitle: 'Event',
+          headerBackButtonDisplayMode: 'minimal',
+          headerTintColor: 'black',
+        }}
+      />
+      <Image source={{ uri: event.image }} className="aspect-video w-full rounded-xl" />
+      <Text className="text-3xl font-bold" numberOfLines={2}>
+        {event.title}
+      </Text>
+      <Text className="text-lg font-semibold uppercase text-amber-800">
+        {dayjs(event.datetime).format('ddd, D MMM')} • {dayjs(event.datetime).format('h:MM A')}
+      </Text>
+      <Text className="text-lg">{event.description}</Text>
     </View>
   );
 }
