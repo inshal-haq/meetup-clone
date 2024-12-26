90ec0154e25616fb601944acc750677ca040be71
Inshal Haq <inshalulhaq11@gmail.com>
2024-12-26 13:41:46 -0600

    Display event date and time using dayjs

--- components/EventListItem.tsx
+++ components/EventListItem.tsx
@@ -1,13 +1,14 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
+import dayjs from 'dayjs';
 import { View, Text, Image } from 'react-native';
 
 export default function EventListItem({ event }) {
   return (
-    <View className="gap-3 p-3">
+    <View className="m-3 gap-3 border-b-2 border-gray-100 pb-3">
       <View className="flex-row gap-4">
         <View className="flex-1 gap-2">
           <Text className="text-lg font-semibold uppercase text-amber-800">
-            Wed 13, Sep • 19:30 CET
+            {dayjs(event.datetime).format('ddd, D MMM')} • {dayjs(event.datetime).format('h:MM A')}
           </Text>
           <Text className="text-xl font-bold" numberOfLines={2}>
             {event.title}
