689ad58ec3fbbba397bb97c7d8aa24d3b7d97a66
Inshal Haq <inshalulhaq11@gmail.com>
2024-12-27 23:03:58 -0600

    Allow user to join event

--- app/event/[id].tsx
+++ app/event/[id].tsx
@@ -3,12 +3,17 @@ import { Stack, useLocalSearchParams } from 'expo-router';
 import { useEffect, useState } from 'react';
 import { View, Text, Image, Pressable, ActivityIndicator } from 'react-native';
 
+import { useAuth } from '~/contexts/AuthProvider';
 import { supabase } from '~/utils/supabase';
 
 export default function EventScreen() {
   const { id } = useLocalSearchParams();
+
   const [event, setEvent] = useState(null);
+  const [attendance, setAttendance] = useState(null);
   const [loading, setLoading] = useState(true);
+
+  const { user } = useAuth();
 
   useEffect(() => {
     fetchEvent();
@@ -17,7 +22,25 @@ export default function EventScreen() {
   const fetchEvent = async () => {
     const { data, error } = await supabase.from('events').select('*').eq('id', id).single();
     setEvent(data);
+
+    const { data: attendanceData } = await supabase
+      .from('attendance')
+      .select('*')
+      .eq('user_id', user.id)
+      .eq('event_id', id)
+      .single();
+    setAttendance(attendanceData);
     setLoading(false);
+  };
+
+  const joinEvent = async () => {
+    const { data, error } = await supabase
+      .from('attendance')
+      .insert({ user_id: user.id, event_id: event.id })
+      .select()
+      .single();
+
+    setAttendance(data);
   };
 
   if (loading) {
@@ -49,9 +72,13 @@ export default function EventScreen() {
       {/* Footer */}
       <View className="absolute bottom-0 left-0 right-0 flex-row items-center justify-between border-t-2 border-gray-300 p-5 pb-10">
         <Text className="text-xl font-semibold">Free</Text>
-        <Pressable className="rounded-md bg-red-400 p-5 px-8">
-          <Text className="text-lg font-bold text-white">Join and RSVP</Text>
-        </Pressable>
+        {attendance ? (
+          <Text className="text-lg font-semibold text-green-500">You are attending!</Text>
+        ) : (
+          <Pressable onPress={() => joinEvent()} className="rounded-md bg-red-400 p-5 px-8">
+            <Text className="text-lg font-bold text-white">Join and RSVP</Text>
+          </Pressable>
+        )}
       </View>
     </View>
   );
