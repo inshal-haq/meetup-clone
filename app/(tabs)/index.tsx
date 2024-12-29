ec0d1fce7981c7cd1732d0902b5dab3b22b14109
Inshal Haq <inshalulhaq11@gmail.com>
2024-12-29 16:57:50 -0600

    Get user location using Expo Location

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
+import * as Location from 'expo-location';
 import { Stack } from 'expo-router';
 import { useEffect, useState } from 'react';
-import { FlatList } from 'react-native';
+import { Alert, FlatList } from 'react-native';
 
 import EventListItem from '~/components/EventListItem';
 import { NearbyEvent } from '~/types/db';
@@ -8,10 +9,28 @@ import { supabase } from '~/utils/supabase';
 
 export default function Events() {
   const [events, setEvents] = useState<NearbyEvent[]>([]);
+  const [location, setLocation] = useState<Location.LocationObject | null>(null);
 
   useEffect(() => {
-    fetchNearbyEvents();
+    async function getCurrentLocation() {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        Alert.alert('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    }
+
+    getCurrentLocation();
   }, []);
+
+  useEffect(() => {
+    if (location) {
+      fetchNearbyEvents();
+    }
+  }, [location]);
 
   // const fetchAllEvents = async () => {
   //   const { data, error } = await supabase.from('events').select('*');
@@ -19,9 +38,13 @@ export default function Events() {
   // };
 
   const fetchNearbyEvents = async () => {
+    if (!location) {
+      return;
+    }
+
     const { data, error } = await supabase.rpc('nearby_events', {
-      lat: 33.124479,
-      long: -96.631919,
+      lat: location.coords.latitude,
+      long: location.coords.longitude,
     });
 
     // console.log(JSON.stringify(data, null, 2));
